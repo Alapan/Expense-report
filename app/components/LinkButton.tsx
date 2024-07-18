7171df47ec57de3efa77f16ada0dc2b135fee71a
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-07-18 22:57:53 +0300

    Add component for expense form and update LinkButton

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -3,11 +3,18 @@ import Link from 'next/link';
 interface ButtonProps {
   linkTo: string;
   buttonText: string;
+  backgroundClass: string;
+  backgroundClassOnHover: string;
 }
 
-const LinkButton = ({ linkTo, buttonText }: ButtonProps) => {
+const LinkButton = ({
+  linkTo,
+  buttonText,
+  backgroundClass,
+  backgroundClassOnHover
+}: ButtonProps) => {
   return (
-    <Link href={linkTo} className='inline-block w-full my-2 lg:w-2/5 lg:ms-6 text-center text-white bg-slate-700 hover:bg-slate-800 hover:underline rounded-lg p-3.5'>{buttonText}</Link>
+    <Link href={linkTo} className={`inline-block w-full my-2 lg:w-2/5 lg:ms-6 text-center text-white ${backgroundClass} ${backgroundClassOnHover} hover:underline rounded-lg p-3.5`}>{buttonText}</Link>
   );
 }
 
