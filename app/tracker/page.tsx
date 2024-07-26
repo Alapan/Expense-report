94e61a4b5d1c3271f2cf026671e11c5c2347455d
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-07-26 23:01:09 +0300

    Create separate form for larger screens

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -1,21 +1,26 @@
 import { NextPage } from 'next';
 import LinkButton from '@/components/LinkButton';
-
+import LargeScreenExpenseForm from '@/components/LargeScreenExpenseForm';
 
 const Page: NextPage = () => {
   return (
-    <div className='fixed bottom-0 w-full border-t-2 border-slate-200 border-solid'>
-      <div className='grid grid-cols-3 grid-rows-1'>
-        <div className='col-start-2'>
-          <LinkButton
-            linkTo='/tracker/expense'
-            buttonText='Add Expense'
-            backgroundColor='bg-lime-700'
-            backgroundColorOnHover='hover:bg-lime-800'
-          />
+    <>
+      <div className='hidden xl:block'>
+        <LargeScreenExpenseForm />
+      </div>
+      <div className='fixed bottom-0 w-full border-slate-200 border-solid xl:hidden'>
+        <div className='grid grid-cols-3 grid-rows-1 md:grid-cols-5'>
+          <div className='col-start-2 md:col-start-3'>
+            <LinkButton
+              linkTo='/tracker/expense'
+              buttonText='Add Expense'
+              backgroundColor='bg-lime-700'
+              backgroundColorOnHover='hover:bg-lime-800'
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
