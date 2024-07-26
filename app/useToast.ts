6e2c6012330072504086270c44834a5c2cefaf4f
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-07-26 22:57:35 +0300

    Use custom hook for controlling Toast visibility

---
+++ app/useToast.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+const useToast = () => {
+  const [ isVisible, setIsVisible ] = useState(false);
+
+  const showToast = () => {
+    setIsVisible(true);
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 5000);
+  };
+
+  return { isVisible, showToast };
+};
+
+export default useToast;
