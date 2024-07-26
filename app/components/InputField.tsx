f87389bc7be72da05abe3f42b3af3414b4a251e6
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-07-26 12:37:24 +0300

    Add InputField component

---
+++ app/components/InputField.tsx
@@ -0,0 +1,23 @@
+interface InputFieldProps {
+  placeholder: string;
+  name: string;
+  label: string;
+  isRequired: boolean;
+};
+
+const InputField = ({ placeholder, name, label, isRequired }: InputFieldProps ) => {
+  return (
+    <>
+      <label className='font-light'>{label}</label>
+      <input
+        type='text'
+        placeholder={`${placeholder}`}
+        name={name}
+        required={isRequired}
+        className='my-4 h-10 border-solid border-2 border-slate-300 rounded-md px-2'
+      />
+    </>
+  );
+};
+
+export default InputField;
