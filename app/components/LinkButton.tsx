4fb6f441f9f614dbd7c9d3ebf2916ca4f37594b6
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-07-26 12:38:10 +0300

    Rename LinkButton prop names

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 interface ButtonProps {
   linkTo: string;
   buttonText: string;
-  backgroundClass: string;
-  backgroundClassOnHover: string;
+  backgroundColor: string;
+  backgroundColorOnHover: string;
 }
 
 const LinkButton = ({
   linkTo,
   buttonText,
-  backgroundClass,
-  backgroundClassOnHover
+  backgroundColor,
+  backgroundColorOnHover
 }: ButtonProps) => {
   return (
-    <Link href={linkTo} className={`inline-block w-full my-2 lg:w-2/5 lg:ms-6 text-center text-white ${backgroundClass} ${backgroundClassOnHover} hover:underline rounded-lg p-3.5`}>{buttonText}</Link>
+    <Link href={linkTo} className={`inline-block w-full my-2 lg:w-2/5 lg:ms-6 text-center text-white ${backgroundColor} ${backgroundColorOnHover} hover:underline rounded-lg p-3.5`}>{buttonText}</Link>
   );
 }
 
