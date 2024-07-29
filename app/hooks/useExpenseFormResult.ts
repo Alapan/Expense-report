d65cd0a788efa1b5c4da8edb6a4bd4ca94c110ca
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-07-29 18:10:10 +0300

    Use router.refresh for expense table and change result type from string to number

--- app/hooks/useExpenseFormResult.ts
+++ app/hooks/useExpenseFormResult.ts
@@ -1,17 +1,20 @@
 import { createRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import useToast from './useToast';
-import { createExpense } from '@/actions';
+import { createExpense } from '../../db/serverActions';
 
 const useExpenseFormResult = () => {
   const ref = createRef<HTMLFormElement>();
-  const [ result, setResult ] = useState('');
+  const [ result, setResult ] = useState(0);
   const { isVisible, showToast } = useToast();
-  
+  const router = useRouter();
+
   const handleSubmit = async (formData: FormData) => {
     const response = await createExpense(formData);
     setResult(response);
-    if (response === 'success' && ref.current) {
+    if (response > 0 && ref.current) {
       ref.current.reset();
+      router.refresh();
     }
     showToast();
   }
