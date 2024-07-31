e38ce506fedcd06fc4eb56bc18f4bf2bc60fc27f
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-07-31 17:07:56 +0300

    Add new components ConfirmationModal and ExpenseTable

---
+++ app/components/ConfirmationModal.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+
+interface ConfirmationModalProps {
+  message: string;
+  header: string;
+  onDelete: () => void;
+  submitBtnCls: string;
+  isModalVisible: boolean;
+  onDismiss: () => void;
+}
+
+const ConfirmationModal = ({
+  message,
+  header,
+  onDelete,
+  submitBtnCls,
+  isModalVisible,
+  onDismiss,
+}: ConfirmationModalProps) => {
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (isModalVisible) {
+      ref.current?.showModal();
+    } else {
+      ref.current?.close();
+    }
+  }, [isModalVisible]);
+
+  const onSubmit = () => {
+    onDelete();
+    onDismiss();
+  }
+
+  return (
+    <dialog
+      ref={ref}
+    >
+      <div className='h-48 w-96 rounded-lg grid grid-rows-3 grid-cols-3'>
+        <div className='row-start-1 col-start-1 col-span-3 border-solid border-b-2 border-slate-200 pt-5 ps-2.5 font-medium text-xl'>
+          {header}
+        </div>
+        <div className='row-start-2 col-start-1 col-span-3 h-20 border-solid border-b-2 border-slate-200 pt-5 ps-2.5 font-light'>
+          {message}
+        </div>
+        <div className='row-start-3 col-start-1 col-span-3'>
+          <div className='grid grid-rows-1 grid-cols-4 mt-6'>
+            <div className='row-start-1 col-start-3 col-span-1 ps-6'>
+              <button
+                className={'rounded-md bg-slate-200 h-9 w-16 font-light'}
+                onClick={onDismiss}
+              >Cancel</button>
+            </div>
+            <div className='row-start-1 col-start-4 col-span-1 ps-2.5'>
+              <button
+                className={`rounded-md ${submitBtnCls} h-9 w-16 font-light`}
+                onClick={onSubmit}
+              >{'Delete'}</button>
+            </div>  
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={onDismiss}
+        className='absolute top-5 right-4'
+      >X</button>
+    </dialog>
+  );
+};
+
+export default ConfirmationModal;
---
+++ app/components/ExpenseTable.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { useState } from 'react';
+import { ExpensesByMonth } from '@/types';
+import ExpenseRow from './ExpenseRow';
+import useToast from '@/hooks/useToast';
+import { deleteExpense } from '@/db/serverActions';
+import Toast from './Toast';
+
+interface ExpenseTableProps {
+  expensesByMonths: ExpensesByMonth[]
+}
+
+const ExpenseTable = ({ expensesByMonths }: ExpenseTableProps) => {
+  const { isVisible, showToast } = useToast();
+  const [ deleteResult, setDeletionResult ] = useState(0);
+
+  const handleDelete = async (expenseId: number) => {
+    const deletedExpenseId = await deleteExpense(expenseId);
+    if (deletedExpenseId === expenseId) {
+      setDeletionResult(deletedExpenseId);
+    } else {
+      setDeletionResult(-1);
+    }
+    showToast();
+  }
+
+  return (
+    <>
+      {expensesByMonths.map(({ month, year, expenses, total }, i) => (
+        <div key={`${month} ${year}`} className='mx-5 my-10'>
+          <div className='text-3xl mb-10 font-medium'>{month} {year}</div>
+          <div className='grid grid-rows-1 grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 pb-10'>
+            <div className='col-start-1 col-span-1 text-3xl font-medium'>
+              {'Total'}
+            </div>
+            <div className='col-start-3 col-span-1 text-3xl md:col-start-4 xl:col-start-5 2xl:col-start-6 font-medium mx-auto'>
+              {`${total}${expenses[0].currency}`}
+            </div>
+          </div>
+          {expenses.map((expense) => (
+            <div key={expense.id} className='mb-10'>
+              <ExpenseRow
+                expense={expense}
+                handleDelete={handleDelete}
+              />
+            </div>
+          ))}
+        </div>
+      ))}
+      {
+        deleteResult > 0 && (
+        <Toast
+          message='Expense successfully deleted!'
+          backgroundColor='bg-green-400'
+          width='w-8/12 md:w-4/12'
+          isVisible={isVisible}
+        />
+      )}
+      {
+        deleteResult < 0 && (
+        <Toast
+          message='Error while deleting!'
+          backgroundColor='bg-red-400'
+          width='w-8/12 md:w-4/12'
+          isVisible={isVisible}
+        />
+      )}
+    </>
+  );
+}
+
+export default ExpenseTable;
