3b08b2cf8a8343095396d845ab6e4d98a09b7499
Alapan Mukherjee <alapan.mukherjee@futurice.com>
2024-09-14 13:09:09 +0300

    Remove unused import

--- __tests__/ExpenseTable.test.tsx
+++ __tests__/ExpenseTable.test.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 
 import ExpenseTable from '@/components/ExpenseTable';
 import { ExpensesByMonth } from '@/types';
